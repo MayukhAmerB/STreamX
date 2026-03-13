fbbe1ebf634368ea03aa3f3b715415e6fb620770
Mayukh Bhattacharya <mayukhofficia@gmail.com>
2026-03-14 00:22:24 +0530

    Guard app layout against background render crashes

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -2,11 +2,14 @@ import { Outlet } from "react-router-dom";
 import AppFooter from "../components/AppFooter";
 import GlobalPageBackground from "../components/GlobalPageBackground";
 import Navbar from "../components/Navbar";
+import SafeRender from "../components/SafeRender";
 
 export default function AppLayout() {
   return (
     <div className="relative min-h-screen bg-black text-white">
-      <GlobalPageBackground />
+      <SafeRender fallback={null}>
+        <GlobalPageBackground />
+      </SafeRender>
       <div className="relative z-10">
         <Navbar />
         <main className="relative">
